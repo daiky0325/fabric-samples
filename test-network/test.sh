3636744d7493b1a9313b1a4f4a01a2fe37c86107
adash333(ニックネームなど) <xxxxx@xxx.com>
2024-10-12 17:40:11 +0900

    修正

--- test-network/test.sh
+++ test-network/test.sh
@@ -3,10 +3,11 @@
 ./network.sh down
 docker volume rm $(docker volume ls -q)docker volume rm $(docker volume ls -q)
 
+./network.sh up
 ./network.sh createChannel 
-./network.sh createChannel -org 1 -c "org1" -ca
-./network.sh createChannel -org 2 -c "org2" -ca
-./network.sh createChannel -org 3 -c "org3" -ca
+./network.sh createChannel -org 1 -c "org1" 
+./network.sh createChannel -org 2 -c "org2" 
+./network.sh createChannel -org 3 -c "org3" 
 
 ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go 
 ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go -c "org1" -org 1
